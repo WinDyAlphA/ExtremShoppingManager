0d0b4392e4fd1d31ec0712a4e5c305d277e301b6
Noah <noah@Noahs-MacBook-Air.local>
2023-03-07 11:54:57 +0100

    commit

--- skeleton/src/Form/UserType.php
+++ skeleton/src/Form/UserType.php
@@ -6,14 +6,13 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Uuid;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('uuid')
-            ->add('roles')
             ->add('pseudo')
             ->add('password')
         ;
