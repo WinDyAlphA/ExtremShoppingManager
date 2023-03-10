a2e7a9e59a3bef5ad2944a56335d5abe8d5fe0a8
Hippo <hippolytebl@gmail.com>
2023-03-10 10:54:25 +0100

    Updating style of home page and list view

--- skeleton/src/Controller/HomeController.php
+++ skeleton/src/Controller/HomeController.php
@@ -15,10 +15,17 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(ListeRepository $listeRepo): Response
     {
+        
         $listes = $this->getUser()->getListe();
+        $total = 0;
+        foreach ($listes as $liste) {
+            $total += $liste->getTotal();
+        }
+        $average = $total / count($listes);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'listes' => $listes,
+            'average' => $average,
         ]);
     }
 }
