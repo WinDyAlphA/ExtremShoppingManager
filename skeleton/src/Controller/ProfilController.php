67a189a1f01285f94b68e7e75d557615feed0ea6
Noah <heraud260@gmail.com>
2023-04-01 01:01:54 +0200

    UX upgraded, TODO afficher les trois premiers produits dans les card listes: cf templates/home/index l:26

---
+++ skeleton/src/Controller/ProfilController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArticleRepository;
+use App\Repository\ContientRepository;
+use App\Repository\ListeRepository;
+use App\Repository\ProposeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+
+class ProfilController extends AbstractController
+{
+    #[Route('/profil', name: 'app_profil')]
+    public function index(ListeRepository $listeRepo,EntityManagerInterface $entityManager,ContientRepository $contientRepo, ProposeRepository $proposeRepo, ArticleRepository $articleRepo): Response
+    {
+        $listes = $this->getUser()->getListe();
+        $pseudo = $this->getUser()->getPseudo();
+        $articles = $articleRepo->findAll();
+        $proposes = $proposeRepo->findAll();
+        $contients = $contientRepo->findAll();
+        if(count($listes) == 0 || count($listes) == null){
+            $counter = 1;
+        } else {
+            $counter = count($listes);
+        }
+        $total = 0;
+        foreach ($listes as $liste) {
+            $total += $liste->getTotal();
+        }
+        $average = $total / $counter;
+        // Round to 2 decimals
+        $average = round($average, 2);
+
+        return $this->render('profil/index.html.twig', [
+            'controller_name' => 'ProfilController',
+            'liste' => $listes,
+            'average' => $average,
+            'pseudo' => $pseudo,
+            'articles' => $articles,
+            'proposes' => $proposes,
+            'contients' => $contients,
+            
+
+        ]);
+    }
+}
